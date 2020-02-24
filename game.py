47b46d0948208f6ed9a84a9fcff434d88a862102
luiz <luiz@mavromati.com>
2020-02-24 13:22:52 -0300

    added collision check

--- game.py
+++ game.py
@@ -44,13 +44,13 @@ class Player():
     def move_left(self, x, y): 
         if(x > bLEFT):
             pygame.draw.polygon(screen, black, ((x, y), (x + 20, y + 30), (x - 20, y + 30)))
-            return x - 0.01, y
+            return x - 0.005, y
         else: return x,y
 
     def move_right(self, x, y): 
         if(x < bRIGHT):
             pygame.draw.polygon(screen, black, ((x, y), (x + 20, y + 30), (x - 20, y + 30)))
-            return x + 0.01, y
+            return x + 0.005, y
         else: return x,y
         
         
@@ -68,41 +68,54 @@ class Bullet():
     def __init__(self, x, y):
         self.x = int(x)
         self.y = int(y)
+        self.alive = True
         self.draw()
         
     def erase(self):
-        pygame.draw.circle(screen, black, (self.x, self.y), 1 )
+        if self.alive: pygame.draw.circle(screen, black, (self.x, self.y), 1 )
         
     def draw(self):
-        pygame.draw.circle(screen, (255,255,255), (self.x, self.y), 1 )
+        if self.alive: pygame.draw.circle(screen, (255,255,255), (self.x, self.y), 1 )
 
+
+#ENEMY CLASS
+class Enemy():
+    def __init__(self, idx, x, y):
+        self.idx = idx
+        self.x = x
+        self.y = y
+        self.alive = True
+        self.instance = None
+        
 #ARMY CLASS
 class Army():
     def __init__(self):
-        self.army = range(50)
+        self.army = list()
         self.x, self.y = int(width/10), 50
+        for e in range(49): self.army.append(Enemy(e, self.x - 30, self.y ))
         self.armyTimer = time.time()
         self.lastMove = 0
         self.thisMove = "left"
         
     
     def drawArmy(self):
-        x = self.x - 30
-        y = self.y
         screen.fill(black)
+        x = self.x-30
+        y = self.y
         for e in self.army:
-            
-            if e != None: pygame.draw.circle(screen, 255, (x, y), 18)
+            if e.idx == 0: pygame.draw.circle(screen, 255, (self.x - 30, self.y), 18)
             x += int(width/11)
-            if e in range(9,50,10): 
+            if e.idx in range(9,50,10): 
                 y += 50
-                x = int(width/10) - 30
+                x = int(width/11) - 22
+        
                 if self.lastMove <= 6: 
                     x = x + self.lastMove * 10
-                elif self.lastMove < 12: 
-                    x = ((self.lastMove) * 10) - 10
-                else:
-                    x = x
+                elif self.lastMove > 6: 
+                    x = (self.lastMove * 10) - 10
+            e.x, e.y = x, y
+            if e.alive: pygame.draw.circle(screen, 255, (e.x, e.y), 18)
+
  
 
     def moveArmy(self, x, y):
@@ -110,17 +123,18 @@ class Army():
         if now > self.armyTimer + 1:
             
             screen.fill(black)
-            
+            #for e in self.army: 
+                
             if self.lastMove < 6:
-                self.x = x + 10
+                self.x = self.x + 10
                 self.drawArmy()
                 self.lastMove += 1
-            elif self.lastMove > 6:
-                self.x = x - 10
+            elif self.lastMove >= 6:
+                self.x = self.x - 10
                 self.drawArmy()
                 self.lastMove -= 1
-            else:
-                self.y = y + 20
+            if self.lastMove == 6:
+                self.y = self.y + 20
                 self.drawArmy()
                 if self.thisMove == "left": 
                     self.lastMove = 12
@@ -129,12 +143,24 @@ class Army():
                     self.lastMove = 0
                     self.thisMove = "left"   
 
-            self.armyTimer = time.time()       
+            self.armyTimer = time.time()
+            
+
 
 #INIT ENTS
 player = Player()
 army = Army()
 
+
+def checkHit(bullets, army):
+    for b in bullets:
+        for e in army:
+            #pygame.draw.polygon(screen, 255, ((e.x-20,e.y-20),(e.x-20,e.y-20),(e.x+20,e.y+20),(e.x+20,e.y-20)))
+            if b.x > e.x - 20 and b.x < e.x + 20 and b.y > e.y - 20 and b.y < e.y + 20 and e.alive and b.alive:
+                e.alive = False
+                b.alive = False
+                bullets.remove(b)
+    
 #GAME LOOP
 while 1:
     
@@ -151,6 +177,8 @@ while 1:
     player.moveBullets()
     
     player.shoot()
+    
+    checkHit(player.bullets, army.army)
     
     army.moveArmy(army.x, army.y)
     pygame.display.flip()
